e46f08ae708f2191ff560a91ed7c7f3925ad1695
GauravPandey123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-10-01 23:33:46 +0530

    update1

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Button() {
+export default function Button({name='',handler,defaultStyle={}}) {
   return (
     <div>
-      
+      <button onClick={handler} style={defaultStyle} >{name||"submit"}</button>
     </div>
   )
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import style from './Home.module.css'
+import Button from './Button'
 export default function Home() {
+    const handleSubbmit=()=>{
+       alert("clicked")
+    }
   return (
-    <div>
-      
+    <div className={style.home}>
+
+      <div className={style.card}>
+        <form>
+       
+        
+        <Button name={"Register"} handler={handleSubbmit} defaultStyle={{"color":"red"}}/>
+        </form>
+      </div>
     </div>
   )
 }
