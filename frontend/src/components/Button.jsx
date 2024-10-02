a6d1f19c37e48220a287498ea29c32371522ee5e
Adnan Khan <beingadnankhan678@gmail.com>
2024-10-02 13:04:42 +0530

    greeting

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Button.css';
 
 export default function Button({name='',handler,defaultStyle={}}) {
   return (
     <div>
-      <button onClick={handler} style={defaultStyle} >{name||"submit"}</button>
+      <button className="button-50" onClick={handler} style={defaultStyle} >{name||"submit"}</button>
     </div>
   )
 }
